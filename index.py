eaa646761c380611025637fa6dc307b3cdde2239
vigneswaran <rnvigneswaran@gmail.com>
2024-11-28 00:05:52 +0530

    sql connect

--- index.py
+++ index.py
@@ -1,4 +1,23 @@
-from flask import Flask , render_template
+from flask import *
+
+import mysql.connector
+
+connection = mysql.connector.connect(
+             host = "127.0.0.1",
+             user = "root",
+             password = "PassworD@0409",
+             database = "gatepass_project"                
+                                     
+)
+
+cursor = connection.cursor()
+
+conquery = """CREATE TABLE IF NOT EXISTS userlog (username VARCHAR(250), password INTEGER);"""
+
+cursor.execute(conquery)
+
+
+
 
 app = Flask(__name__)
 
@@ -9,5 +28,5 @@ def home():
     return render_template("index.html")
 
 
-
-app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug = True)
